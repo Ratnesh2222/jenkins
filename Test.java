2b3624e18f21db01010791ad7980469b3b718abd
Ratnesh2222 <112641179+Ratnesh2222@users.noreply.github.com>
2024-09-02 10:55:07 +0530

    Update Test.java --2

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test{
   public static void main(String s[]) {
     System.out.println("Jai mata di");
     System.out.println("Welcome to word of jenkins");
-    System.out.println("triiger from webhook--1");
+    System.out.println("triiger from webhook--2");
   }
 }
